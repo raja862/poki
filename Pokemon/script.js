2057a023252d0dd7f51b9bc075fba2d9a82ec80f
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2022-08-11 18:03:05 +0530

    Add files via upload

---
+++ Pokemon/script.js
@@ -0,0 +1,36 @@
+let pokedex = document.getElementById('pokedex');
+
+let fetchPokemon = () => {
+    let promises = [];
+    for (let i = 1; i <= 50; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        let pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            id: result.id
+        }));
+        displayPokemon(pokemon);
+    });
+};
+
+let displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    let pokemonHTMLString = pokemon
+        .map(
+            (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+        </li>
+    `
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
